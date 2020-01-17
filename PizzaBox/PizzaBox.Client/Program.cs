61985034ff79af9f58f71f82bf7d46dbb900f7ad
Rodney Canlas <canlasrj@gmail.com>
2020-01-17 00:50:42 -0600

    blackout

--- PizzaBox/PizzaBox.Client/Program.cs
+++ PizzaBox/PizzaBox.Client/Program.cs
@@ -11,29 +11,7 @@ namespace PizzaBox.Client
         static void Main(string[] args)
         {
 
-            bool isAdmin = false;
-            bool validUser = false;
-            bool loggedIn = false;
-
-            UIHandling.Intro();
-            if (UIHandling.ReturningMember())
-            {
-                UIHandling.SigningIn(ref isAdmin, ref validUser, ref loggedIn);
-            }
-            else
-            {
-                UIHandling.CreatingAccount();
-                UIHandling.SigningIn(ref isAdmin, ref validUser, ref loggedIn);
-            }
-            while (!validUser)
-            {
-                UIHandling.CreatingAccount();
-                UIHandling.SigningIn(ref isAdmin, ref validUser, ref loggedIn);
-            }
-            if (validUser && loggedIn)
-            {
-                Console.WriteLine("You are logged in!");
-            }
+            
         }
     }
 }
